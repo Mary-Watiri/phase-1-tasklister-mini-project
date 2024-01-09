c60384d9910efa1c8a4b942ab888d448f38d1d1e
Mary Watiri <watirimary30@gmail.com>
2024-01-09 21:40:01 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const tasksList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskValue = taskInput.value.trim();
+    if(taskValue !== ''){
+
+      const listItem = document.createElement('li');
+      listItem.textContent = taskValue;
+      tasksList.appendChild(listItem);
+      taskInput.value = '';
+    };
+  });
 });
